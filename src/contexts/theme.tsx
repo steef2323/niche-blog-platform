5012d2920ed71639fafaf5a8d01b7bfedb1fa1b2
steef2323 <rembrandtvanwine@gmail.com>
2025-07-09 09:25:57 +0200

    Update theme context configuration

--- src/contexts/theme.tsx
+++ src/contexts/theme.tsx
@@ -27,7 +27,7 @@ export function ThemeProvider({ site, children }: { site: Site | null; children:
           }
         }
       });
-    }
+    } 
   }, [site]);
 
   const value = React.useMemo(() => ({
